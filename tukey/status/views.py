2952beec7f720ba65c59cf760d1cbb6eb9808a77
mtgreenway <mgreenway@uchicago.edu>
2013-04-01 17:51:26 -0500

    status fixed up requires local_settings

--- tukey/status/views.py
+++ tukey/status/views.py
@@ -1,9 +1,9 @@
 # Create your views here.
+from django.conf import settings
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+
 import urllib2, json
-
-
 
 def get_percent(used, total):
     if float(total) == 0:
@@ -12,61 +12,71 @@ def get_percent(used, total):
         return float(used)/float(total)
 
 def append_status(data, cloud_status_data, currcloud, label, idprefix, value_name):
-    cloud_status_data.append( (label, idprefix + '-' + value_name, data[currcloud][value_name]['val'], data[currcloud][value_name]['max'], get_percent(data[currcloud][value_name]['val'], data[currcloud][value_name]['max']) ) )
+    max_val =  data[currcloud][value_name]['max']
+    val =  data[currcloud][value_name]['val']
+    cloud_status_data.append((label, idprefix + '-' + value_name, val, max_val,
+        get_percent(val, max_val)))
 
 def status_public(request):
-    status_req = urllib2.Request('http://dashboard.opensciencedatacloud.org/state.json')
+    ''' Requires settings.STATUS_URL and settings.STATUS where settings.STATUS
+    is a list of tuples consisting of a display name and a dictionary with three
+    entries: dashboard, idprefix, and type. For Example:
+        STATUS = [
+            ("Adler", {
+                "dashboard": "OSDC Cloud2",
+                "idprefix": "adler",
+                "type": "cloud"
+            }),
+            ("OCC-Y", {
+                "dashboard": "OCC-Y",
+                "idprefix": "occ-y",
+                "type": "map_reduce"
+            }),
+            ("OCC-Root", {
+                "dashboard": "OCC-Root",
+                "idprefix": "occ-root",
+                "type": "storage"})
+        ]
+    TODO: need to get the storage portion worked out'''
+
+    status_req = urllib2.Request(settings.STATUS_URL)
     opener = urllib2.build_opener()
     data = json.loads(str(opener.open(status_req).read()), 'utf-8')
-    
-    cloud_names = ['Adler', 'Sullivan', 'OCC-Y', 'OCC-Root']
 
     update_times = {}
     status_data = {}
 
-    #There's gotta be a better way to do this -- probably fix the json... ok this is slightly better for now.
-    for cloud_name in cloud_names:
-        status_data[cloud_name] = []
-        standard = False
-        
-        if(cloud_name == 'Adler'):
-            currcloud = 'OSDC Cloud2'
-            idprefix = 'adler'
-            standard = True
-        elif(cloud_name == 'Sullivan'):
-            currcloud = 'OSDC-Sullivan'
-            idprefix = 'sullivan'
-            standard = True
-        elif(cloud_name == 'OCC-Y'):
-            currcloud = 'OCC-Y'
-            idprefix = 'occ-y'
-            standard = False
-        elif(cloud_name =='OCC-LVOC'):
-            currcloud = 'OCC-LVOC-HADOOP'
-            idprefix = 'occ-lvoc'
-            standard = False
-        elif(cloud_name == 'OCC-Root'):
-            continue
+    status_attrs = {
+        "cloud": [
+            ('Storage Used (GB): ', 'cluster'),
+            ('Active Users: ', 'users'), ('VM Instances: ', 'vms'),
+            ('VM Cores: ', 'cores'), ('VM RAM (GB): ', 'ram'),
+            ('VM Instance Storage (GB): ', 'ldisk')],
+        "map_reduce": [
+            ('HDFS Storage (GB): ', 'hdfsdu'),
+            ('Active Jobs: ', 'jobs'), ('Active Users: ', 'users')],
+        "storage" : [
+            ('Storage Used (GB): ', 'disk')]
+        }
 
-        if currcloud in data:    
-            if standard:
-                append_status(data, status_data[cloud_name], currcloud, 'Storage Used (GB): ', idprefix, 'cluster')
-                append_status(data, status_data[cloud_name], currcloud, 'Active Users: ', idprefix, 'users')
-                append_status(data, status_data[cloud_name], currcloud, 'VM Instances: ', idprefix, 'vms')
-                append_status(data, status_data[cloud_name], currcloud, 'VM Cores: ', idprefix, 'cores')
-                append_status(data, status_data[cloud_name], currcloud, 'VM RAM (GB): ', idprefix, 'ram')
-                append_status(data, status_data[cloud_name], currcloud, 'VM Instance Storage (GB): ', idprefix, 'ldisk')
-            else:
-                append_status(data, status_data[cloud_name], currcloud, 'HDFS Storage (GB): ', idprefix, 'hdfsdu')
-                append_status(data, status_data[cloud_name], currcloud, 'Active Jobs: ', idprefix, 'jobs')
-                append_status(data, status_data[cloud_name], currcloud, 'Active Users: ', idprefix, 'users')
-            
-            
-            update_times[cloud_name] = data[currcloud]['users']['stsh']
+    for cloud_name, cloud in settings.STATUS:
+        if cloud["type"] == "storage":
+            continue
+        status_data[cloud_name] = []
+
+        if "idprefix" in cloud and "dashboard" in cloud and cloud["dashboard"] in data:
+
+            for attr_name, attr_id in status_attrs[cloud["type"]]:
+                print cloud["idprefix"], cloud["dashboard"]
+                append_status(data, status_data[cloud_name],
+                    cloud["dashboard"], attr_name, cloud["idprefix"], attr_id)
+                print status_data[cloud_name]
+
+            update_times[cloud_name] = data[cloud["dashboard"]]['users']['stsh']
         else:
             update_times[cloud_name] = "None"
 
-    #special for root here        
+    #special for root here
     storage_names = ['OCC-Root']
 
     old_ncbi_total = 333832
@@ -94,10 +104,15 @@ def status_public(request):
 
             status_data[storage_name].append( ('Storage Used (GB): ', idprefix + '-' + value_name, storage_total, max_storage, storage_percent) )
             update_times[storage_name] = status_time
-            print('storage_total: ' + str(storage_total) + ' max_storage: ' + str(max_storage))
+            #print('storage_total: ' + str(storage_total) + ' max_storage: ' + str(max_storage))
         else:
             update_times[storage_name] = "None"
 
-    print(status_data)
+    #print(status_data)
 
-    return render_to_response('status/status_public.html', {'status_data' : status_data, 'cloud_names' : cloud_names, 'update_times' : update_times}, context_instance=RequestContext(request))
+    return render_to_response('status/status_public.html', {
+        'status_data': status_data, 'cloud_names': [n for n, _ in settings.STATUS],
+        'update_times': update_times},
+        context_instance=RequestContext(request))
+
+
