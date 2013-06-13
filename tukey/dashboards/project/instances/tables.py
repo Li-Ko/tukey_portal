18e9bdfdfee4ee26b6b6e6dda3865e8b72d53df0
mtgreenway <mgreenway@uchicago.edu>
2013-06-13 22:08:47 +0000

    the cluster terminate

--- tukey/dashboards/project/instances/tables.py
+++ tukey/dashboards/project/instances/tables.py
@@ -1,13 +1,16 @@
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
+from openstack_dashboard import api
+
 from horizon import tables
 
 from tukey.cloud_attribute import get_cloud
 
 from openstack_dashboard.dashboards.project.instances.tables import (
     InstancesTable as OldInstancesTable,
-    AssociateIP, SimpleAssociateIP, SimpleDisassociateIP, EditInstance, TerminateInstance)
+    AssociateIP, SimpleAssociateIP, SimpleDisassociateIP, EditInstance,
+    TerminateInstance, ConsoleLink)
 
 
 class LaunchLink(tables.LinkAction):
@@ -15,12 +18,33 @@ class LaunchLink(tables.LinkAction):
     verbose_name = _("Launch Instance")
     url = "horizon:project:images_and_snapshots:index"
 
+class TerminateCluster(tables.BatchAction):
+    name = "terminate_cluster"
+    action_present = _("Terminate")
+    action_past = _("Scheduled termination of")
+    data_type_singular = _("Cluster")
+    data_type_plural = _("Clusters")
+    classes = ('btn-danger', 'btn-terminate')
+
+    def allowed(self, request, instance=None):
+        if instance:
+           # this doesn't feel right
+           self.instance = instance
+           return instance.name.startswith("torque-node-") or \
+               instance.name.startswith("torque-headnode-")
+    
+    def action(self, request, obj_id):
+        #need to different
+        new_id = "cluster%s-%s" % (get_cloud(self.instance).lower(), obj_id)
+        api.server_delete(request, new_id)
+
 class InstancesTable(OldInstancesTable):
 
     cloud = tables.Column(get_cloud, verbose_name=_("Cloud"))
 
     Meta = OldInstancesTable.Meta
     Meta.table_actions = (LaunchLink, TerminateInstance)
+    Meta.row_actions = (TerminateCluster,) + OldInstancesTable.Meta.row_actions
 
 
 old_edit_instance_allowed = EditInstance.allowed
@@ -61,4 +85,10 @@ def simple_disassociate_ip_allowed(self, request, instance=None):
         and old_simple_disassociate_ip_allowed(self, request, instance)
 
 SimpleDisassociateIP.allowed = simple_disassociate_ip_allowed
+
+def console_link_allowed(self, request, instance=None):
+    return False
+
+ConsoleLink.allowed = console_link_allowed
+
 
