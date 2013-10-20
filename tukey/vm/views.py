6f7c17402b354144f192ac4997dc35d5b9c250d6
mtgreenway <mgreenway@uchicago.edu>
2013-10-20 23:57:49 +0000

    Need to login to rproxy

--- tukey/vm/views.py
+++ tukey/vm/views.py
@@ -1,13 +1,14 @@
-# Create your views here.
-from django.conf import settings
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from django.template.defaultfilters import slugify
+#from django.conf import settings
+#from django.shortcuts import render_to_response
+#from django.template import RequestContext
+#from django.template.defaultfilters import slugify
 from django.views.decorators.csrf import csrf_exempt
+from horizon.decorators import require_auth
 
-import urllib2, json
+
 from revproxy import proxy
 
+@require_auth
 @csrf_exempt
 def reverse_proxy(request, ip, path):
     request.path = path
