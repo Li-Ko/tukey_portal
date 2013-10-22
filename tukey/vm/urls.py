4ca9ace0b64e43323440fe236799ee59b8de2bb0
mtgreenway <mgreenway@uchicago.edu>
2013-10-22 18:36:49 +0000

    added cloud

--- tukey/vm/urls.py
+++ tukey/vm/urls.py
@@ -3,6 +3,6 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('tukey.vm.views',
     #url(r'^(?P<ip>.+)/(?P<port>\d+)/(?P<path>\w*)$', 'reverse_proxy'),
-    url(r'^(?P<ip>.+)/(?P<path>\w*)$', 'reverse_proxy'),
+    url(r'^(?P<cloud>\w*)/(?P<ip>.+)/(?P<path>\w*)$', 'reverse_proxy'),
 )
 
--- tukey/vm/views.py
+++ tukey/vm/views.py
@@ -4,13 +4,15 @@
 #from django.template.defaultfilters import slugify
 from django.views.decorators.csrf import csrf_exempt
 from horizon.decorators import require_auth
-
+from django.http import Http404
 
 from revproxy import proxy
 
 @require_auth
 @csrf_exempt
-def reverse_proxy(request, ip, path):
-    request.path = path
-    return proxy.proxy_request(request, destination="http://%s" % ip)
+def reverse_proxy(request, cloud, ip, path):
+    request.path = path[-1] if path.endswith("/") else path
+    if cloud.lower() == "sullivan":
+        return proxy.proxy_request(request, destination="http://%s" % ip)
+    raise Http404
 
