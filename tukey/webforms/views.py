c0d7e7dcfc0a1a66cafe946db4bf46b9c9bf886d
Matt Greenway <mgreenway@uchicago.edu>
2014-02-05 14:02:30 -0600

    handle non-email federated-identifiers

--- tukey/webforms/views.py
+++ tukey/webforms/views.py
@@ -1,15 +1,18 @@
+import smtplib
+
 from django.conf import settings
+from django.contrib.auth import logout
+from django.core.exceptions import ValidationError
 from django.core.mail import EmailMessage
 from django.core.mail import send_mail, BadHeaderError
+from django.core.validators import validate_email
 from django.forms.util import ErrorList
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django_openid_auth.views import parse_openid_response
 from openid.consumer.consumer import SUCCESS
-from tukey.webforms.forms import OSDCForm, OSDCSupportForm, OSDCDemoForm
-import smtplib
 from tukey.openid_auth import pre_apply
-from django.contrib.auth import logout
+from tukey.webforms.forms import OSDCForm, OSDCSupportForm, OSDCDemoForm
 
 def build_message(form):
     msg_list = []
@@ -60,10 +63,10 @@ def build_message(form):
     msg_list.append(form.cleaned_data['projectname'])
     msg_list.append('\n\nProject Description\n')
     msg_list.append(form.cleaned_data['projectdescr'])
-   
+
     if (form.cleaned_data['sharing'] != ""):
         msg_list.append('\n\nSharing with:\n')
-	msg_list.append(form.cleaned_data['sharing'])
+        msg_list.append(form.cleaned_data['sharing'])
 
     msg_list.append('\n\nProject Lead\n')
     msg_list.append(form.cleaned_data['projectlead'])
@@ -73,13 +76,13 @@ def build_message(form):
     msg_list.append(form.cleaned_data['cpus'])
 
     if form.cleaned_data['more_cpus'] != "":
-	msg_list.append("(Specific requirements: " + form.cleaned_data['more_cpus'] + ")")    
+        msg_list.append("(Specific requirements: " + form.cleaned_data['more_cpus'] + ")")
 
     msg_list.append('\n\nEstimated storage:\n')
     msg_list.append(form.cleaned_data['storage'])
 
     if form.cleaned_data['more_storage'] != "":
-	msg_list.append("(Specific requirements: " + form.cleaned_data['more_storage'] + ")")
+        msg_list.append("(Specific requirements: " + form.cleaned_data['more_storage'] + ")")
 
     msg_list.append('\n\nHeard about OSDC from:\n')
     msg_list.append(form.cleaned_data['referral_source'])
@@ -95,41 +98,49 @@ def osdc_apply(request, user=None):
         if form.is_valid(): # All validation rules pass
             subject = 'OSDC Account Request'
             message_admin = build_message(form)
-            sender_admin = form.cleaned_data['email'] 
+            sender_admin = form.cleaned_data['email']
             recipients_admin = [settings.APPLICATION_EMAIL]
-	   
+
             # Values for confirmation email to user ('Subject' remains same)
-	    message_user = "Thank you for your application to the OSDC. " 
-	    message_user += "Someone from our team will contact you within one business day.\n\n%s" % message_admin
-	    sender_user = 'noreply@opensciencedatacloud.org'
+            message_user = "Thank you for your application to the OSDC. "
+            message_user += "Someone from our team will contact you within one business day.\n\n%s" % message_admin
+            sender_user = 'noreply@opensciencedatacloud.org'
             recipients_user = [sender_admin]
 
             email_admin = EmailMessage(subject, message_admin, sender_admin, recipients_admin)
-	    email_user = EmailMessage(subject, message_user, sender_user, recipients_user)
+            email_user = EmailMessage(subject, message_user, sender_user, recipients_user)
 
             if "pubkey" in request.FILES:
                 pubkey = request.FILES["pubkey"]
                 email_admin.attach(pubkey.name, pubkey.read(), pubkey.content_type)
-		email_user.attach(pubkey.name, pubkey.read(), pubkey.content_type)
-                
+                email_user.attach(pubkey.name, pubkey.read(), pubkey.content_type)
+
             try:
                 email_admin.send()
                 email_user.send()
                 if not request.user.is_authenticated():
                     logout(request)
-    		# Redirect after POST
+                    # Redirect after POST
                 return HttpResponseRedirect('thanks/')
 
             except smtplib.SMTPRecipientsRefused as e:
                 form._errors["email"] = ErrorList(
-		    # Changed 'sender' to 'sender_admin' after code split into two emails.
+                    # Changed 'sender' to 'sender_admin' after code split into two emails.
                     [u"Domain of address %s does not exist" % sender_admin])
 
     else:
         if request.user.is_authenticated():
-            form = OSDCForm(initial={"eppn": user.username, "method": "re-apply"})
+            form = OSDCForm(initial={"eppn": user.username,
+                    "method": "re-apply"})
         elif hasattr(user, 'identifier'):
-            form = OSDCForm(initial={"eppn": user.identifier, "email": user.identifier, "method": user.method})
+            try:
+                email_value = user.identifier
+                validate_email(email_value)
+            except ValidationError:
+                email_value = ""
+
+            form = OSDCForm(initial={"eppn": user.identifier,
+                    "email": email_value, "method": user.method})
         else:
             return HttpResponseRedirect('/pre_apply/?next=/apply/')
 
